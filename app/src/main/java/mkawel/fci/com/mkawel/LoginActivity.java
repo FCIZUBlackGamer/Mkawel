472155627f48251d7d08bfb318877f22039388df
fci <momen.shahen2020@gmail.com>
2018-11-25 07:28:43 +0200

    vcs

--- app/src/main/java/mkawel/fci/com/mkawel/LoginActivity.java
+++ app/src/main/java/mkawel/fci/com/mkawel/LoginActivity.java
@@ -1,12 +1,15 @@
 package mkawel.fci.com.mkawel;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -34,7 +37,7 @@ import io.realm.Realm;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView register;
+    TextView register, forget_pass;
     EditText email, password;
     Button login;
     User user;
@@ -46,6 +49,7 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         register = findViewById(R.id.register);
+        forget_pass = findViewById(R.id.forget_pass);
         login = findViewById(R.id.login);
         Realm.init(this);
         final Realm realm = Realm.getDefaultInstance();
@@ -147,5 +151,81 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        forget_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = new EditText(LoginActivity.this);
+                editText.setHint("ادخل بريدك الالكترونى");
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setView(editText)
+                        .setPositiveButton("موافق", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+                                progressDialog.setMessage("جارى إلغاء الصفقة ...");
+                                progressDialog.setCancelable(false);
+                                progressDialog.show();
+                                StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://abdelkreimahmed.000webhostapp.com/ForgetPass.php", new Response.Listener<String>() {
+                                    @SuppressLint("ResourceType")
+                                    @Override
+                                    public void onResponse(String response) {
+                                        Log.e("Response", response);
+                                        progressDialog.dismiss();
+                                        try {
+                                            JSONObject object = new JSONObject(response);
+                                            Toast.makeText(LoginActivity.this, object.getString("res"), Toast.LENGTH_SHORT).show();
+
+                                        } catch (Exception e) {
+
+                                            Toast.makeText(LoginActivity.this, "خطأ فى صيغة الاستقبال", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        progressDialog.dismiss();
+                                        if (error instanceof ServerError)
+                                            Toast.makeText(LoginActivity.this, "خطأ فى الاتصال بالخادم", Toast.LENGTH_SHORT).show();
+                                        else if (error instanceof TimeoutError)
+                                            Toast.makeText(LoginActivity.this, "خطأ فى مدة الاتصال", Toast.LENGTH_SHORT).show();
+                                        else if (error instanceof NetworkError)
+                                            Toast.makeText(LoginActivity.this, "شبكه الانترنت ضعيفه حاليا", Toast.LENGTH_SHORT).show();
+                                    }
+                                }) {
+                                    @Override
+                                    protected Map<String, String> getParams() throws AuthFailureError {
+                                        HashMap<String, String> map = new HashMap<>();
+                                        map.put("Content-Type", "application/json; charset=utf-8");
+                                        map.put("email", editText.getText().toString() + "");
+                                        return map;
+                                    }
+                                };
+                                stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                                        DefaultRetryPolicy.DEFAULT_TIMEOUT_MS,
+                                        3,  // maxNumRetries = 2 means no retry
+                                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                                Volley.newRequestQueue(LoginActivity.this).add(stringRequest);
+                                ViewGroup parent = (ViewGroup) editText.getParent();
+                                if (parent != null) {
+                                    parent.removeAllViews();
+                                }
+                                dialog.dismiss();
+                            }
+                        }).setNegativeButton("لا", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ViewGroup parent = (ViewGroup) editText.getParent();
+                        if (parent != null) {
+                            parent.removeAllViews();
+                        }
+                        dialog.dismiss();
+                    }
+                }).show();
+                ViewGroup parent = (ViewGroup) editText.getParent();
+                if (parent != null) {
+                    parent.removeAllViews();
+                }
+            }
+        });
     }
 }
